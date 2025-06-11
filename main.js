97c996dfc9465af8d35de65b0285040e2a6312c1
Yaroslav Davydchuk <86869015+ua-rik@users.noreply.github.com>
2025-06-11 22:31:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  // Завантажуємо HTML
   const html = await fetch("https://ua-rik.github.io/bunker-express/game.html")
     .then(res => res.text());
-
   const container = document.createElement("div");
   container.innerHTML = html;
   document.body.appendChild(container);
 
-  // Завантажуємо JSON зі сценами
   const scenes = await fetch("https://ua-rik.github.io/bunker-express/scenes.json")
     .then(res => res.json());
-
-  // Передаємо сцени в гру (якщо твоя логіка очікує scenes глобально)
   window.__SCENES__ = scenes;
 
-  // Завантажуємо саму гру
   const script = document.createElement("script");
   script.src = "https://ua-rik.github.io/bunker-express/game-logic.js";
   document.body.appendChild(script);
 });
+
